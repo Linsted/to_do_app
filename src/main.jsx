d58c63795c911b7ae9238f5aff10483567aa3548
Linsted <ivan.malenko18@gmail.com>
2023-10-12 08:48:26 +0300

    fix: update Root path

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./app/App";
 import "./index.css";
-import Root from "./app/root.jsx";
+import Root from "./app/Root.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Root>
